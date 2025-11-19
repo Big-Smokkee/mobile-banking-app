c4ea910c823f97be9db3f52478bca7958a315384
Big-Smokkee <fardinfaraz13579@gmail.com>
2025-11-19 12:51:53 +0600

    feat: add function for button toggle effect and add class name for all form button

--- toggle.js
+++ toggle.js
@@ -1,5 +1,8 @@
-
+//HTML collections
 const formElement = document.getElementsByClassName("forms");
+const formBtn = document.getElementsByClassName("form-btn");
+console.log(formBtn);
+
 //function for toggling
 function toggleFeature(id) {
     for (const form of formElement) {
@@ -7,29 +10,44 @@ function toggleFeature(id) {
     }
     document.getElementById(id).style.display = "block";
 }
+// toogle effect code function
+function toggleEffect(id) {
+    for (const btn of formBtn) {
+        btn.style.backgroundColor = "white";
+        btn.style.border = "1px solid #0808081a";
+    }
+    document.getElementById(id).style.backgroundColor = "#f4f5f7";
+    document.getElementById(id).style.border = "2px solid green";
+
+}
 //add money section
 document.getElementById("add-button").addEventListener("click", function () {
     toggleFeature("add-money-section");
+    toggleEffect("add-button");
 
 });
 //cashout section
 document.getElementById("cash-out-button").addEventListener("click", function () {
     toggleFeature("cash-out-section");
+    toggleEffect("cash-out-button");
 });
 //transfer section
 document.getElementById("transfer-button").addEventListener("click", function () {
-
     toggleFeature("transfer-section");
+    toggleEffect("transfer-button");
 });
 //Get Bonus section
 document.getElementById("get-bonus-button").addEventListener("click", function () {
     toggleFeature("get-bonus-section");
+    toggleEffect("get-bonus-button");
 });
 //Pay Bill section
 document.getElementById("paybill-button").addEventListener("click", function () {
     toggleFeature("pay-bill-section");
+    toggleEffect("paybill-button");
 });
 //Transaction section
 document.getElementById("transaction-button").addEventListener("click", function () {
     toggleFeature("transaction-section");
+    toggleEffect("transaction-button");
 });
